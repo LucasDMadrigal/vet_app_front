d8a552b40f8d612ce7f9b9ee4e57442032898910
Lucas Madrigal <lucasmadrigalb@gmail.com>
2025-03-03 20:19:49 -0300

    wip

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -1,36 +1,49 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import AuthLayout from '../layout/AuthLayout';
-import NewServiceForm from '../components/NewServiceForm';
-import EditServicePrice from '../components/EditServicePrice';
-import NewVeterinarian from '../components/NewVeterinarian';
-import DeleteVeterinarian from '../components/DeleteVaterinarian';
-
+import React from "react";
+import { useSelector } from "react-redux";
+import AuthLayout from "../layout/AuthLayout";
+import NewServiceForm from "../components/NewServiceForm";
+import EditServicePrice from "../components/EditServicePrice";
+import NewVeterinarian from "../components/NewVeterinarian";
+import DeleteVeterinarian from "../components/DeleteVaterinarian";
 
 const AdminPanel = () => {
-
-  const token = useSelector(store =>  store.auth.token);
+  const token = useSelector((store) => store.auth.token);
 
   return (
     <>
       <AuthLayout>
-        <main className="flex flex-wrap justify-center items-start gap-2 min-h-[90vh] p-4 mb-4" >
-          <h1 className='text-5xl text-white font bold w-11/12 text-center bg-[#8BA8C4] py-4 rounded mt-10'  >Admin Panel</h1>
-          <div className='w-11/12 lg:w-8/12 xl:w-4/12 2xl:2/12 flex flex-col justify-center items-center'>
-            <h3 className='text-3xl text-white font-bold w-11/12 text-center bg-[#8BA8C4] py-1 rounded mt-14 mb-4' >Create a new service</h3>
-            <NewServiceForm />
-          </div>
-          <div className='w-11/12 lg:w-8/12 xl:w-4/12 2xl:2/12 flex flex-col justify-center items-center'>
-            <h3 className='text-3xl text-white font-bold w-11/12 text-center bg-[#8BA8C4] py-1 rounded mt-14 mb-4' >Add new veterinarian</h3>
-            <NewVeterinarian />
-          </div>
-          <div className='w-11/12 lg:w-8/12 xl:w-4/12 2xl:2/12 flex flex-col justify-center items-center'>
-            <h3 className='text-3xl text-white font-bold w-11/12 text-center bg-[#8BA8C4] py-1 rounded mt-14 mb-4' >Update service Price</h3>
-            <EditServicePrice />
-          </div>
-          <div className='w-11/12 lg:w-8/12 xl:w-4/12 2xl:2/12 flex flex-col justify-center items-center'>
-            <h3 className='text-3xl text-white font-bold w-11/12 text-center bg-[#8BA8C4] py-1 rounded mt-14 mb-4' >Delete a veterinarian</h3>
-            <DeleteVeterinarian />
+        <main className="flex flex-wrap justify-center items-start gap-2 min-h-[90vh] px-40 mb-4">
+          <h1 className="text-5xl text-white font bold w-full text-center bg-[#8BA8C4] py-4 rounded mt-10">
+            Admin Panel
+          </h1>
+          
+          <div className="grid grid-rows-2 divide-y-2 divide-solid divide-[#8BA8C4] w-full">
+            <section className="py-3">
+              <h2>Veterinary</h2>
+              <button
+                className="inline-flex justify-center items-center min-w-40 px-4 mr-4 py-2 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 hover:text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Create Veterinary
+              </button>
+              <button
+                className="inline-flex justify-center items-center min-w-40 px-4 mr-4 py-2 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 hover:text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Edit Veterinary
+              </button>
+            </section>
+            <section className="py-3">
+              <h2>Services</h2>
+              <button
+                className="inline-flex justify-center items-center min-w-40 px-4 mr-4 py-2 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 hover:text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Create Service
+              </button>
+              <button
+                className="inline-flex justify-center items-center min-w-40 px-4 mr-4 py-2 mt-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 hover:text-white  hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Edit Service
+              </button>
+            </section>
           </div>
         </main>
       </AuthLayout>
@@ -38,6 +51,4 @@ const AdminPanel = () => {
   );
 };
 
-export default AdminPanel
-
-
+export default AdminPanel;
